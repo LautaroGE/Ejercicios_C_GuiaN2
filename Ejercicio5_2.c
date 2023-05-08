e265faeb88d9cdfd024471ea5258e77b292acbad
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-08 04:14:29 -0300

    Ejercicio 5

---
+++ Ejercicio5_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //consigna
+    //Resolver los siguientes problemas mediante programas en C.
+    //Escribir un programa que lea dos enteros que representan una fecha (mes y día del mes)
+    //y escriba true si la fecha corresponde al día de navidad y false de lo contrario.
+    //Ingresar tres valores decimales, calcular e imprimir el mínimo.
+    //Ingresar tres datos de tipo carácter (letras), escribirlos ordenados alfabéticamente.
+
+
+
+
+
+}
