49369b8902d638be931b1684a353faad1c35a260
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-08 03:56:08 -0300

    Ejercicio e ingreso de datos

---
+++ Ejercicio3_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //consigna
+    /* Resolver los siguientes Ítems: Escribir un programa que permita evaluar la siguiente función
+    definida por tramos para un valor de x ingresado por teclado:
+
+    f(x) = {5x + 2 si <=3
+            -------------
+            4x - 5 si x > x
+    */
+
+    int x;
+
+    printf ("Ingrese el valor de x: ");
+    fflush(stdin);
+    scanf("%d", &x);
+
+
+
+
+
+}
