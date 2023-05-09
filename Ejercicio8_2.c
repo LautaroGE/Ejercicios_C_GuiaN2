12cd2709275f1b15be17f78ff5d96fa6dc9c5e0f
Lautaro Garcia <lautaroegarcia1995@gmail.com>
2023-05-08 22:58:47 -0300

    ejercicio 8

---
+++ Ejercicio8_2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    //Consigna
+    // Completar el siguiente fragmento de programa, indicando  además que salidas produce para
+    //los lotes de prueba dados:
+    /*Lote de prueba: a=4
+      Lote de prueba: a=9
+      Lote de prueba: a=12
+      Lote de prueba: a=35
+
+
+    */
+
+    scanf("%d",&a);
+
+    if (a / 2 == 0){
+
+        printf("%d es múltiplo de %d",a, .........);
+
+
+    if (a / 3 == 0) {
+
+        printf("y también de %d \n", ........);
+
+        printf("por lo tanto lo es de%d \n", .......);
+
+            } else {
+
+                printf("pero no de %d \n",..….);
+              }
+
+    } else if (a / 3 == 0) {
+
+         printf("%d es múltiplo de %d pero no de %d", a, ….,….);
+
+      } else {
+
+         printf("%d no es múltiplo de %d ni de %d", a,…,….);
+        }
+
+}
